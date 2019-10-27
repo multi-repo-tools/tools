5333a137a85da4551096939b9df69e929ae09818
Adib Rastegarnia <adib.rastegarnia@gmail.com>
2019-10-27 19:36:38 -0400

    Read config file from args (#3)

--- cmd/linter/linter.go
+++ cmd/linter/linter.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/multi-repo-tools/tools/pkg/utils"
 )
 
 func main() {
 
+	args := os.Args
 	fmt.Println("linter")
 	utils.RunCommand("curl", "-sfL", "https://install.goreleaser.com/github.com/golangci/golangci-lint.sh")
 	utils.RunCommand("sh", "-s", "--", "-b", "$(go env GOPATH)/bin latest")
-	utils.RunCommand("golangci-lint", "run", "--config", "./configs/linter/.golangci.yml")
+	utils.RunCommand("golangci-lint", "run", "--config", args[1])
 }
